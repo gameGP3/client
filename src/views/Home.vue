67db873e7d8d73fad5b817aff5f55abadce06dac
afifahrahmak <afifahrk96@gmail.com>
2019-12-12 17:31:37 +0700

    add userform countdown another

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,108 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div id="homepage">
+      <br />
+      <br />
+      <!-- <sui-icon name="spinner" size="huge" loading /> -->
+      <img src="../assets/image1.jpg" alt style="width:300px" />
+      <h1>Ka-Boot !</h1>
+      <UsernameForm></UsernameForm>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+// import HelloWorld from '@/components/HelloWorld.vue'
+import db from "../config/firestore";
+import UsernameForm from "@/components/UsernameForm";
 export default {
-  name: 'home',
+  name: "Home",
   components: {
-    HelloWorld
+    // HelloWorld
+    UsernameForm
+  },
+  data() {
+    return {
+      name: "",
+      user: "",
+      roomID: "",
+      score: 0
+    };
+  },
+  methods: {
+    show() {
+      db.collection("rooms")
+        .doc(this.$store.state.roomID)
+        .onSnapshot(querySnapshot => {
+          let data = querySnapshot.data();
+          console.log(data, "ini hasilnya");
+        });
+    },
+    createRoom() {
+      this.$store.dispatch("createRoom", this.name);
+      // this.name = ''
+    },
+    joinRoom() {
+      let payload = {
+        id: this.roomID,
+        user: this.name
+      };
+      this.$store.dispatch("joinRoom", payload);
+    },
+    updateScore() {
+      // this.roomID = localStorage.getItem('roomID')
+      let payload = {
+        id: localStorage.getItem("roomID"),
+        score: 5,
+        username: this.name
+      };
+      console.log(payload);
+      this.$store.dispatch("updateScore", payload);
+    }
+  }
+};
+</script>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css?family=Caveat+Brush|Covered+By+Your+Grace|Gloria+Hallelujah|Holtwood+One+SC|Patrick+Hand+SC&display=swap");
+#homepage {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  -webkit-box-align: center;
+  -webkit-box-pack: center;
+  height: 100vh;
+  width: 100%;
+  background-color: rgb(26, 26, 26);
+}
+#homepage h1 {
+  /* font-family: 'Gloria Hallelujah', cursive; */
+  font-family: "Caveat Brush", cursive;
+  /* font-family: 'Gloria Hallelujah', cursive;
+    font-family: 'Gloria Hallelujah', cursive; */
+  animation: 3s ease 0s infinite alternate none running logos;
+  font-size: 70px;
+  margin-top: 0;
+  font-size: 100px;
+}
+@keyframes logos {
+  0% {
+    color: crimson;
+  }
+  25% {
+    color: orangered;
+  }
+  50% {
+    color: goldenrod;
+  }
+  75% {
+    color: black;
+  }
+  100% {
+    color: darkcyan;
   }
 }
-</script>
+</style>
---
+++ src/views/Main.vue
@@ -0,0 +1,96 @@
+<template>
+  <div id="main">
+    <h1 v-if="!raceNow && !ready">vroom vroom !</h1>
+    <div v-if="!raceNow && !ready" class="startDiv">
+      <sui-button size="huge" animated @click.prevent="start">
+        <sui-button-content visible>START</sui-button-content>
+        <sui-button-content hidden>
+          <sui-icon name="play" />
+        </sui-button-content>
+      </sui-button>
+    </div>
+    <h1 v-if="!raceNow && !ready">click click !</h1>
+    <Countdown v-if="ready"></Countdown>
+    <Race id="race" v-if="raceNow"></Race>
+    <audio autoplay loop>
+      <source src="../assets/Final Fantasy VII Chocobo Theme.mp3" type="audio/mp3" />
+    </audio>
+  </div>
+</template>
+
+<script>
+import Countdown from "@/components/Countdown";
+import Race from "@/components/Race";
+import db from "../config/firestore";
+export default {
+  name: "Main",
+  components: {
+    Countdown,
+    Race
+  },
+  data() {
+    return {
+      ready: false,
+      raceNow: false
+    };
+  },
+  methods: {
+    start() {
+      this.ready = true;
+      db.collection("rooms")
+        .doc(this.$store.state.roomID)
+        .update({ status: true })
+        .then(() => {
+          // this.started = true
+          // this.check = true
+          this.$store.commit("UPDATE_STATUS", true);
+        })
+        .catch(err => {
+          this.$store.commit("UPDATE_STATUS", false);
+        });
+      setTimeout(this.race, 3500);
+    },
+    race() {
+      this.ready = false;
+      this.raceNow = true;
+    }
+  }
+};
+</script>
+
+<style scoped>
+#main {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+.startDiv {
+  height: 46vh;
+  width: 29.94%;
+  background-image: url("../assets/bugatti_gif.gif");
+  background-size: contain;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+@media only screen and (max-width: 800px) {
+  #main {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+  }
+  .startDiv {
+    background-size: cover;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 50vh;
+    width: 50%;
+    background-image: url("../assets/bugatti_gif.gif");
+  }
+}
+</style>
