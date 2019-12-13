043a00e6bb904b383bdbddfd73ee0de25331129b
Trayand <patradyn@gmail.com>
2019-12-13 09:01:04 +0700

    doing template

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,28 +1,21 @@
 <template>
   <div>
-  <div id="homepage">
-    <br />
-    <br />
-    <!-- <sui-icon name="spinner" size="huge" loading /> -->
-    <img src="../../../old-config/assets/image1.jpg" alt style="width:300px" />
-    <h1>Ka-Boot !</h1>
-    <UsernameForm></UsernameForm>
-  </div>
+    <div>
+
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue'
 import db from '../config/firestore'
-import UsernameForm from '@/components/UsernameForm'
+import UsernameForm from '../components/UsernameForm'
+
 export default {
   name: 'Home',
   components: {
-    // HelloWorld
     UsernameForm
   },
-  data () {
+  data(){
     return {
       name: '',
       user: '',
@@ -30,83 +23,13 @@ export default {
       score: 0
     }
   },
-  methods: {
-    show () {
-      db.collection('rooms').doc(this.$store.state.roomID).onSnapshot(querySnapshot => {
-        let data = querySnapshot.data()
-        console.log(data, 'ini hasilnya')
-      })
-    },
-    createRoom () {
-      this.$store.dispatch('createRoom', this.name)
-      // this.name = ''
-    },
-    joinRoom () {
-      let payload = {
-        id: this.roomID,
-        user: this.name
-      }
-      this.$store.dispatch('joinRoom', payload)
-    },
-    updateScore () {
-      // this.roomID = localStorage.getItem('roomID')
-      let payload = {
-        id: localStorage.getItem('roomID'),
-        score: 5,
-        username: this.name
-      }
-      console.log(payload)
-      this.$store.dispatch('updateScore', payload)
-    }
+  methods:{
+    
   }
 }
+
 </script>
 
-<style scoped>
-@import url("https://fonts.googleapis.com/css?family=Caveat+Brush|Covered+By+Your+Grace|Gloria+Hallelujah|Holtwood+One+SC|Patrick+Hand+SC&display=swap");
+<style>
 
-#homepage {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-  -webkit-box-align: center;
-  -webkit-box-pack: center;
-  height: 100vh;
-  width: 100%;
-  background-color: rgb(26, 26, 26);
-}
-
-#homepage h1 {
-  /* font-family: 'Gloria Hallelujah', cursive; */
-  font-family: "Caveat Brush", cursive;
-  /* font-family: 'Gloria Hallelujah', cursive;
-    font-family: 'Gloria Hallelujah', cursive; */
-  animation: 3s ease 0s infinite alternate none running logos;
-  font-size: 70px;
-  margin-top: 0;
-  font-size: 100px;
-}
-
-@keyframes logos {
-  0% {
-    color: crimson;
-  }
-  25% {
-    color: orangered;
-  }
-
-  50% {
-    color: goldenrod;
-  }
-
-  75% {
-    color: black;
-  }
-
-  100% {
-    color: darkcyan;
-  }
-}
 </style>
