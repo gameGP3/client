a66c937017ece17ac93a9ce49e237f56da04d919
Trayand <patradyn@gmail.com>
2019-12-13 09:21:20 +0700

    make-home-page 00

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,35 +1,110 @@
 <template>
   <div>
-    <div>
-
+    <div id="homepage">
+      <img src="../../../old-config/assets/image1.jpg" alt style="width:300px" />
+      <h1>Ka-Boot !</h1>
+      <UsernameForm></UsernameForm>
     </div>
   </div>
 </template>
 
 <script>
-import db from '../config/firestore'
-import UsernameForm from '../components/UsernameForm'
+import db from "../config/firestore";
+import UsernameForm from "../components/UsernameForm";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
     UsernameForm
   },
-  data(){
+  data() {
     return {
-      name: '',
-      user: '',
-      roomID: '',
+      name: "",
+      user: "",
+      roomID: "",
       score: 0
-    }
+    };
   },
-  methods:{
-    
+  methods: {
+    show() {
+      db.collection("rooms")
+        .doc(this.$store.state.roomID)
+        .onSnapshot(querySnapshot => {
+          let data = querySnapshot.data();
+          console.log(data, "ini hasilnya");
+        });
+    },
+    createRoom() {
+      this.$store.dispatch("createRoom", this.name);
+      // this.name = ''
+    },
+    joinRoom() {
+      let payload = {
+        id: this.roomID,
+        user: this.name
+      };
+      this.$store.dispatch("joinRoom", payload);
+    },
+    updateScore() {
+      // this.roomID = localStorage.getItem('roomID')
+      let payload = {
+        id: localStorage.getItem("roomID"),
+        score: 5,
+        username: this.name
+      };
+      console.log(payload);
+      this.$store.dispatch("updateScore", payload);
+    }
   }
-}
-
+};
 </script>
 
-<style>
+<style scoped>
+@import url("https://fonts.googleapis.com/css?family=Caveat+Brush|Covered+By+Your+Grace|Gloria+Hallelujah|Holtwood+One+SC|Patrick+Hand+SC&display=swap");
+/* font type mau di ganti kah ? */
 
+
+#homepage {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  -webkit-box-align: center;
+  -webkit-box-pack: center;
+  height: 100vh;
+  width: 100%;
+  background-color: rgb(26, 26, 26);
+}
+
+#homepage h1 {
+  /* font-family: 'Gloria Hallelujah', cursive; */
+  font-family: "Caveat Brush", cursive;
+  /* font-family: 'Gloria Hallelujah', cursive;
+    font-family: 'Gloria Hallelujah', cursive; */
+  animation: 3s ease 0s infinite alternate none running logos;
+  font-size: 70px;
+  margin-top: 0;
+  font-size: 100px;
+}
+
+@keyframes logos {
+  0% {
+    color: crimson;
+  }
+  25% {
+    color: orangered;
+  }
+
+  50% {
+    color: goldenrod;
+  }
+
+  75% {
+    color: black;
+  }
+
+  100% {
+    color: darkcyan;
+  }
 </style>
